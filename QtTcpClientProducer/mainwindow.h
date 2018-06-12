44a308c7e4562629ab45180ef34a21bf38719713
Jadson Bezerra <jadson013@gmail.com>
2018-06-12 15:44:14 -0300

    .

--- QtTcpClientProducer/mainwindow.h
+++ QtTcpClientProducer/mainwindow.h
@@ -7,10 +7,13 @@
 #include <QTimerEvent>
 #include <vector>
 
+
 namespace Ui {
 class MainWindow;
 }
-
+/**
+ * @brief A classe MainWindow é a classe que gera a janela principal
+ */
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
@@ -21,15 +24,42 @@ public:
   explicit MainWindow(QWidget *parent = 0);
   void timerEvent(QTimerEvent *event);
   ~MainWindow();
+  /**
+   * @brief O métoodo putData joga os dados pro servidor
+    */
   void putData();
 public slots:
+  /**
+   * @brief O Metodo tcpConnect conecta a janela ao servidor
+   */
   void tcpConnect();
+  /**
+   * @brief O Metodo tcpDisconnect desconecta a janela ao servidor
+   */
   void tcpDisconnect();
+  /**
+   * @brief O Metodo changeIP muda o IP do servidor conectado
+   */
   void changeIP(QString _IP);
+  /**
+   * @brief O Metodo setMinData muda o minimo dos dados aleatorios
+   */
   void setMinData(int _min);
+  /**
+   * @brief O Metodo setMaxData muda o maximo dos dados aleatorios
+   */
   void setMaxData(int _max);
+  /**
+   * @brief O Metodo ligaTimer liga o timer
+   */
   void ligaTimer();
+  /**
+   * @brief O Metodo desligaTimer desliga o timer
+   */
   void desligaTimer();
+  /**
+   * @brief O Metodo setTimer configura o timer
+   */
   void setTimer(int _t);
 private slots:
 
